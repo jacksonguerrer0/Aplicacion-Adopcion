672f7ef7d56b256b0569a87673d98a47408586c4
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-06-03 07:41:57 -0500

    Mejorado algoritmo de carga index.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,5 @@
-const Carga = ()=>{
-    let cargar = document.querySelector("#text-cargar").value;
-    if(cargar = "Cargando..."){
-        setTimeout("window.location.href='waiting-1.html'", 5000);
-    }
-    
-}
-Carga();
+const URL = window.location.pathname;
+console.log(URL)
+if(URL === '/index.html') {
+    setTimeout("window.location.href='./waiting-1.html'", 5000);
+};
