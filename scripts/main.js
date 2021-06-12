cbbf64ded06595fa52cf6d16e2dc6d243489544b
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-06-12 18:50:52 -0500

    Categoria y llamando apis

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,21 @@
+import Category from "./category.js"
 
-
-
+//llamo a categorias
+const category = new Category;
+const divCategoria = document.querySelector('#categoria');
 
 const URL = window.location.pathname;
 console.log(URL)
 if(URL === '/index.html') {
     setTimeout("window.location.href='./waiting-1.html'", 5000);
 };
+
+
+category.showCategory();
+
+divCategoria.addEventListener("click", e =>{
+    let idCategoria = category.clickCategoria(e);
+
+    let showPets = category.consultaId(idCategoria);
+
+})
