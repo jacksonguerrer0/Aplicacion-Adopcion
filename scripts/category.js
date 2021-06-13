51893b67d046e3e01d88ad88626ddc9d99dfc301
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-06-13 16:48:10 -0500

    Mejorando diseño

--- scripts/category.js
+++ scripts/category.js
@@ -1,7 +1,7 @@
-const perro = document.querySelector('#perro'); 
-const gato = document.querySelector('#gato');
 const divCategoria = document.querySelector('#categoria');
-const pets = document.querySelector('#pets')
+const pets = document.querySelector('#pets');
+
+
 export default class Category{
     showCategory(){
     const Url = 'https://my-json-server.typicode.com/hispanos/db-adopcion/categories'
@@ -10,26 +10,20 @@ export default class Category{
     .then(data => {
     data.forEach(elemento => {
         divCategoria.innerHTML += 
-        `<div id="${elemento.id}" class="perro">
+        `<div id="${elemento.id}" class="divCategoria">
             <img class="circle" src="./img/${elemento.image}" alt="">
             <p>${elemento.name}</p>
         </div>
         `
     });
-
     })
-    .catch(error => console.log(error))
+    .catch(error => console.log("Error en la funcion showCategory" + error))
     }
     clickCategoria(e){
-        let idCategoria = ""; 
-
-            if(e.target.classList[0] === "perro" ){
-                idCategoria = e.target.id;
-            }else if(e.target.parentElement.classList[0] === "perro"){
-                idCategoria = e.target.parentElement.id;
-            }
-        
-        return idCategoria;
+        let idCategoria = "";
+        if(e.target.classList[0] === "divCategoria" )idCategoria = e.target.id;
+        else if(e.target.parentElement.classList[0] === "divCategoria") idCategoria = e.target.parentElement.id;
+        return idCategoria
     }
     consultaId(id){
         let Url = "https://my-json-server.typicode.com/hispanos/db-adopcion/pets";
@@ -37,11 +31,11 @@ export default class Category{
         .then(response => response.json())
         .then(data => {
             let dataFilter =  data.filter((elemento)=>elemento.categoryId === id);
+            console.log(dataFilter)
             this.pintarPets(dataFilter)
         })
     }
     pintarPets(data){
-        console.log(data)
         data.forEach(element => {
             pets.innerHTML += `
             <p>Nombres ${element.name}</p>
--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,25 @@
 import Category from "./category.js"
 
-//llamo a categorias
+//llamo a la clase category
 const category = new Category;
+
 const divCategoria = document.querySelector('#categoria');
 
+
+//Pagina de espera
 const URL = window.location.pathname;
 console.log(URL)
 if(URL === '/index.html') {
     setTimeout("window.location.href='./waiting-1.html'", 5000);
 };
 
-
+//Mostrar categoria Div Perro y Gato
 category.showCategory();
 
+
+//al hacer click en divCategoria
 divCategoria.addEventListener("click", e =>{
     let idCategoria = category.clickCategoria(e);
-
     let showPets = category.consultaId(idCategoria);
-
+    console.log(showPets)
 })
