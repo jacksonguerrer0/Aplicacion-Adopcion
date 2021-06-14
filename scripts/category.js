3c9386be743789b8b74127e40cae6065ebb8bfa6
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-06-14 13:00:04 -0500

    Mostrar categoria y detalle

--- scripts/category.js
+++ scripts/category.js
@@ -1,9 +1,11 @@
 const divCategoria = document.querySelector('#categoria');
 const pets = document.querySelector('#pets');
+const divDetalle = document.querySelector('.content')
 
 
 
 export default class Category{
+    //Mostrar en el div de categoria
     showCategory(){
     const Url = 'https://my-json-server.typicode.com/hispanos/db-adopcion/categories'
     fetch(Url)
@@ -14,85 +16,101 @@ export default class Category{
         `<div id="${elemento.id}" class="divCategoria">
             <img class="circle" src="./img/${elemento.image}" alt="">
             <p>${elemento.name}</p>
-        </div>
-        `
+        </div>`
     });
     })
     .catch(error => console.log("Error en la funcion showCategory" + error))
     }
+    //Muestra el id del div  donde se hace click
     clickCategoria(e){
         let idCategoria = "";
         if(e.target.classList[0] === "divCategoria" )idCategoria = e.target.id;
-        else if(e.target.parentElement.classList[0] === "divCategoria") idCategoria = e.target.parentElement.id;
+        if(e.target.parentElement.classList[0] === "divCategoria") idCategoria = e.target.parentElement.id;
         return idCategoria
     }
+    //Filtra segun el id
     consultaId(id){
         let Url = "https://my-json-server.typicode.com/hispanos/db-adopcion/pets";
         fetch(Url)
         .then(response => response.json())
         .then(data => {
-            let dataFilterPerro =  data.filter((elemento)=>elemento.categoryId === "category-dogs");
-            let dataFilterGato =  data.filter((elemento)=>elemento.categoryId === "category-cats");
-            this.pintarPets(id, dataFilterPerro, dataFilterGato);
+            let dataFilter =  data.filter((elemento)=>elemento.categoryId === id);
+            this.pintarPets(id, dataFilter);
         })
     }
-
-    pintarPets(id, dataPerro, dataGato){
-        if(id === "category-dogs"){
-            let perro = document.querySelector(".cardPerro");
-                dataPerro.forEach(element => {
+    //Mostrar siempre en inicio
+    showPets(){
+        pets.innerHTML=""
+        let Url = "https://my-json-server.typicode.com/hispanos/db-adopcion/pets";
+        fetch(Url)
+        .then(response => response.json())
+        .then(data => {
+            let dataFilter =  data.filter((elemento)=>elemento.categoryId === "category-dogs");
+            dataFilter.forEach(element => {
+                pets.innerHTML += 
+                `<div class="card" id="${element.id}" style="background-image: url(${element.image});">
+                    <div class="cardGrad">
+                        <p class="nombre">${element.name}</p>
+                        <p class="race">${element.race}</p>
+                    </div>
+                </div>`
+            })
+        })
+    }
+    //Pintar segun el id
+    pintarPets(id, dataFilter){
+        console.log(id, dataFilter)
+        if(id === "category-dogs"){ 
+            pets.innerHTML=""
+                dataFilter.forEach(element => {
                     pets.innerHTML += 
-                    `<div class="card cardPerro"  style="background-image: url(${element.image});">
+                    `<div class="card" id="${element.id}" style="background-image: url(${element.image});">
                         <div class="cardGrad">
                             <p class="nombre">${element.name}</p>
                             <p class="race">${element.race}</p>
                         </div>
                     </div>`
-                    if(element.categoryId === "category-dogs"){
-                        perro.style.display = "";
-                        console.log("es perro")
-                    }else if (element.categoryId === "category-cats"){
-                        console.log("es cat")
-                    } 
                 })
-
+        }else if (id === "category-cats") {
+            pets.innerHTML = ""
+                dataFilter.forEach(element => {
+                    pets.innerHTML += 
+                    `<div class="card" id="${element.id}"  style="background-image: url(${element.image});">
+                        <div class="cardGrad">
+                            <p class="nombre">${element.name}</p>
+                            <p class="race">${element.race}</p>
+                        </div>
+                    </div>`
+                })
         }
-        console.log(id, dataPerro, dataGato)
     }
-            // dataPerro.forEach(element => {
-               
-
-            // });
-        //     if (element.categoryId === "category-dogs") {
-        //         pets.innerHTML += `
-        //         <div class="card cardPerro"  style="background-image: url(${element.image});">
-        //             <div class="cardGrad">
-        //                 <p class="nombre">${element.name}</p>
-        //                 <p class="race">${element.race}</p>
-        //             </div>
-        //         </div>`
-
-        //     } 
-        //     else if (element.categoryId === "category-cats") {
-        //         return 
-        //     }
-
-  
-
-
-    // `   pets.innerHTML += 
-    //             <div class="card cardPerro"  style="background-image: url(${element.image});">
-    //                 <div class="cardGrad">
-    //                     <p class="nombre">${element.name}</p>
-    //                     <p class="race">${element.race}</p>
-    //                 </div>
-    //             </div>`
-
-    //             pets.innerHTML += 
-    //             <div class="card" id="cardGato" style="background-image: url(${element.image});">
-    //                 <div class="cardGrad">
-    //                     <p class="nombre">${element.name}</p>
-    //                     <p class="race">${element.race}</p>
-    //                 </div>
-    //             </div>`
+    //Obtener el id de la tarjeta
+    idPets(e){
+        let idPet = "";
+        if(e.target.className === "card" )idPet = e.target.id;
+        else if(e.target.parentElement.className === "card") idPet = e.target.parentElement.id;
+        else if (e.target.parentElement.parentElement.className === "card") idPet =  e.target.parentElement.parentElement.id;
+        return idPet
+    }
+    //Pinta el detalle de la mascota
+    showIdPets(idPet){
+        console.log(idPet)
+        divDetalle.innerHTML=""
+        let Url = "https://my-json-server.typicode.com/hispanos/db-adopcion/pets";
+        fetch(Url)
+        .then(response => response.json())
+        .then(data => {
+            let dataFilter =  data.filter((elemento)=>elemento.id === idPet);
+            console.log(dataFilter)
+            dataFilter.forEach(element => {
+                divDetalle.innerHTML += 
+                `<div class="card" id="${element.id}" style="background-image: url(${element.image});">
+                    <div class="cardGrad">
+                        <p class="nombre">${element.name}</p>
+                        <p class="race">${element.race}</p>
+                    </div>
+                </div>`
+            })
+        })
+    }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,52 @@ import Category from "./category.js"
 
 //llamo a la clase category
 const category = new Category;
-
 const divCategoria = document.querySelector('#categoria');
+const pets = document.querySelector('#pets')
 
 
 //Pagina de espera
 const URL = window.location.pathname;
 console.log(URL)
+
+
+
 if(URL === '/index.html') {
     setTimeout("window.location.href='./waiting-1.html'", 5000);
-};
-
-//Mostrar categoria Div Perro y Gato
-category.showCategory();
+}
 
 
-//al hacer click en divCategoria
-divCategoria.addEventListener("click", e =>{
-    let idCategoria = category.clickCategoria(e);
-    let showPets = category.consultaId(idCategoria);
 
-})
+if(URL === "/category.html"){
+    //Mostrar categoria Div Perro y Gato
+    category.showCategory();
+    category.showPets();
+
+
+    //al hacer click en divCategoria
+    divCategoria.addEventListener("click", e =>{
+        let idCategoria = category.clickCategoria(e);
+        let showPets = category.consultaId(idCategoria);
+    })
+
+
+    //al hacer click en cualquier card Pet
+    pets.addEventListener("click", e =>{
+        let idPet = category.idPets(e);
+        window.location.href="../detalle.html?idPets="+ idPet;
+    })
+
+}
+
+
+
+if(URL === "/detalle.html"){
+    console.log(location.search)
+    let detalle = ()=>{
+        let parametro = new URLSearchParams(location.search)
+        let idPet = parseInt(parametro.get("idPets"))
+        let showIdPet = category.showIdPets(idPet)
+    }
+    detalle()
+
+}
