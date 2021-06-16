9e878e6c76c031285473956d53149331de034ddb
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-06-15 22:40:21 -0500

    Diseño con mediaQuery

--- scripts/message.js
+++ scripts/message.js
@@ -4,14 +4,16 @@ export default class Message{
         let obteniendo = JSON.parse(localStorage.getItem("Favoritos"));
         obteniendo.forEach((element) => {
             content.innerHTML = `
+            <div id="mContent">
             <div class="mHeader">
                 <button onclick="location.href='category.html'" class="dDevolver"></button>
                 <img class="dImgAutor" src="./img/autor.png" alt="">
                 <h4>${element.autor}</h4>
-            </div>
-            <form class="mForm">
+                <form class="mForm">
                 <input type="text" class="mText">
-            </form>`
+            </form>
+            </div></div>
+`
         })
 
     }
